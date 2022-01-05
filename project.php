0a4ea539af262d1a1e5c4fe7032a59f6df63e104
David Tessier <david.tessier2@edu.devinci.fr>
2022-01-05 15:23:09 +0100

    :construction: Adding stuff to project page

--- project.php
+++ project.php
@@ -68,6 +68,88 @@
 
     </section>
 
+    <div class="buffer"></div>
+
+<section class="project reverse">
+
+    <div>
+        <div>
+            <img src="img/studio_canal_logo.png" alt="">
+            <h1>STUDIO CANAL</h1>
+            <p>Application de vente de Video on Demand Studio Canal. Le but du projet est de permettre aux gens de pouvoir voir les films acheter en blu-ray et dvd sur une appli iPad. Nous avons participé à la création de  l’application et du webservice qui se trouve derrière.
+
+ </p></div>
+        <div>
+            <h1>Notre solution</h1>
+            <p>Technique utilisée: Application Native iOS</p>
+            <p>Spécifications: 
+            -Slider d’images et de textes<br>
+            -Système de recherche<br>
+            -Système de video on demand<br>
+            -Téléchargement de film<br>
+            -Sécurisation des transferts et protection contre piratage.
+            </p>
+        </div>
+    </div>
+    <div>
+        <img src="img/studio_canal_pres.png" alt="">
+    </div>
+
+</section>
+
+
+<div class="buffer"></div>
+
+<section class="project">
+
+    <div>
+        <div>
+            <img src="img/hd_vison_logo.png" alt="">
+            <h1>HD VISION</h1>
+            <p>Application HD Vision permettant au magazine de me endigital publishing tous ses numéros et de les classer.</p></div>
+        <div>
+            <h1>Notre solution</h1>
+            <p>Technique utilisée: Application Native iOS</p>
+            <p>Spécifications:<br> 
+            -Slider de chapitrage<br> 
+            -Système de vision avant/ après par curseur<br> 
+            -Système d’implémentation vidéo<br> 
+            -Système d’implémentation et mise en page magazine.
+
+            </p>
+        </div>
+    </div>
+    <div>
+        <img src="img/hd_vison_pr.png" alt="">
+    </div>
+
+</section>
+
+
+<div class="buffer"></div>
+
+<section class="project reverse">
+
+    <div>
+        <div>
+            <h1>PARIS VILLE LUMIERE</h1>
+            <p>Application permettant aux touristes de se promener et d’admirer Paris au travers d’une collection de photographies d’exception. La disponibilité d’une carte 3D permettant de localiser ces monuments est un grand plus de l’application.</p></div>
+        <div>
+            <h1>Notre solution</h1>
+            <p>Technique utilisée: Application Native iOS</p>
+            <p>Spécifications:<br> 
+            -Localisation des monuments sur carte 3D<br> 
+            -Slider photo<br> 
+            -Géolocalisation sur carte 2D<br> 
+            -Navigation simplifié par outils d’orientation<br> 
+            </p>
+        </div>
+    </div>
+    <div>
+        <img src="img/paris_pr.png" alt="">
+    </div>
+
+</section>
 
 
 
